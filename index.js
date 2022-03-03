4895b012075adffe91f399ea5ce51fe277eba326
Danial tarmizi <danial.tarmizi459@gmail.com>
2022-03-04 00:12:18 +0800

    parse arr to string & revert node-fetch ver

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const fetch = require('node-fetch');
 const bodyparser = require("body-parser");
+const fetch = require('node-fetch');
 const app = express();
 
 app.use(bodyparser.urlencoded({ extended: true }));
@@ -51,21 +51,33 @@ app.post("/", function (req, res) {
   var myEpoch2 = Math.trunc(myEpoch)
 
   const date_arr_add = [];
+  const unix_epoch_arr = [];
+
   date_arr_add.push(a_date.toUTCString());
+  unix_epoch_arr.push(myEpoch2);
 
   for (let i = -10; i >=-60 ; i-=10) {
     c_date = add_minutes(a_date,i);
     date_arr_add.push(c_date.toUTCString());
+
+    var myEpoch = c_date.getTime()/1000;
+    var myEpoch2 = Math.trunc(myEpoch)
+    unix_epoch_arr.push(myEpoch2);
   }
 
   date_arr_add.reverse();
+  unix_epoch_arr.reverse();
 
   for (let i = 10; i <=60 ; i+=10) {
     b_date = add_minutes(a_date,i);
     date_arr_add.push(b_date.toUTCString());
+
+    var myEpoch = b_date.getTime()/1000;
+    var myEpoch2 = Math.trunc(myEpoch)
+    unix_epoch_arr.push(myEpoch2);
   }
 
-  console.log(date_arr_add);
+  //console.log(unix_epoch_arr.join());
 
   // res.send(b_date.toUTCString());
   
@@ -75,6 +87,25 @@ app.post("/", function (req, res) {
 
   // })
 
+  url = "https://api.wheretheiss.at/v1/satellites/25544/positions?timestamps="+unix_epoch_arr.join();
+
+
+  // (async () => {
+  //   try {
+  //     const res = await fetch(url);
+  //     const headerDate = res.headers && res.headers.get('date') ? res.headers.get('date') : 'no response date';
+  //     console.log('Status Code:', res.status);
+  //     console.log('Date in Response header:', headerDate);
+  
+  //     const users = await res.json();
+  //     for(user of users) {
+  //       console.log(`Got user with id: ${user.footprint}, name: ${user.timestamp}`);
+  //     }
+  //   } catch (err) {
+  //     console.log(err.message); //can be console.error
+  //   }
+  // })();
+  
   //res.send(myDate);
 //   res.send("Date: "+date.getDate()+
 //           "/"+(date.getMonth()+1)+
