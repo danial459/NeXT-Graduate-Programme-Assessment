c6450b973d8b183cefbcfa842bda5ff2455d3ce0
Danial tarmizi <danial.tarmizi459@gmail.com>
2022-03-07 09:42:53 +0800

    adding comment

--- index.js
+++ index.js
@@ -17,18 +17,23 @@ app.set('view engine','ejs');
 const path = __dirname + '/views/';
 const port = 8090;
 
-app.get('/', (req, res)=> {
+//route to main page
+app.get('/', (req, res)=> { 
   res.render('home')
 })
 
+//route for date submission
 app.post("/", function (req, res) {
  
+  //receive date request from view
   date = req.body.date;
   time = req.body.time;
  
+  //split into array
   const dateArr = date.split("-");
   const timeArr = time.split(":");
 
+  //split the array into variable
   year = dateArr[0];
   month = parseInt(dateArr[1])-1;
   day = dateArr[2];
@@ -36,42 +41,54 @@ app.post("/", function (req, res) {
   hour = timeArr[0];
   minute = timeArr[1];
 
+  //function for converting date and adding 10 minutes
   var add_minutes =  function (dt, minutes) {
     return new Date(dt.getTime() + (minutes*60000));
   }
 
+  //converting variables into date format (UTC +0)
   a_date = new Date(Date.UTC(year, month, day, hour, minute, 0));
 
+  //converting date into Unix timestamp
   var myEpoch = a_date.getTime()/1000;
   var myEpoch2 = Math.trunc(myEpoch)
 
+  //initialize date array and Unix timestamp array
   const date_arr_add = [];
   const unix_epoch_arr = [];
 
+  //conveting date in human readable format and push into date array
   date_arr_add.push(a_date.toUTCString());
   unix_epoch_arr.push(myEpoch2);
 
+  
   for (let i = -10; i >=-60 ; i-=10) {
+    //decrement of 10 minutes on date and convert to human readable format and push into date array
     c_date = add_minutes(a_date,i);
     date_arr_add.push(c_date.toUTCString());
 
+    //convert date into Unix timestamp and push into Unix timestamp array
     var myEpoch = c_date.getTime()/1000;
     var myEpoch2 = Math.trunc(myEpoch)
     unix_epoch_arr.push(myEpoch2);
   }
 
+  //reverse the postion for both array
   date_arr_add.reverse();
   unix_epoch_arr.reverse();
 
   for (let i = 10; i <=60 ; i+=10) {
+    //increment of 10 minutes on date and convert to human readable format and push into date array
     b_date = add_minutes(a_date,i);
     date_arr_add.push(b_date.toUTCString());
 
+    //convert date into Unix timestamp and push into Unix timestamp array
     var myEpoch = b_date.getTime()/1000;
     var myEpoch2 = Math.trunc(myEpoch)
     unix_epoch_arr.push(myEpoch2);
   }
  
+//function for merging both array to be json format to show in view
 const mapArrays = (date_arr_add, unix_epoch_arr) => {
    const res = [];
    for(let i = 0; i < date_arr_add.length; i++){
@@ -83,16 +100,20 @@ const mapArrays = (date_arr_add, unix_epoch_arr) => {
    return res;
 };
 
+//call the function to merge both array
 table_arr = mapArrays(date_arr_add, unix_epoch_arr);
 
+  //converting Unix epoch array to human readable and merge with ISS api url
   url = "https://api.wheretheiss.at/v1/satellites/25544/positions?timestamps="+unix_epoch_arr.join();
   
+  //send the output to home view
   res.render('home', {
     url,
     table_arr
 
   })
 
+  //fetch the longitude,latitude from ISS api and using openGeocoder api to get the locatioon
   fetch(url)
   .then(res => res.json())
   .then(json => {
@@ -105,6 +126,7 @@ table_arr = mapArrays(date_arr_add, unix_epoch_arr);
       .reverse(json[i].longitude, json[i].latitude)
       .end((err, res) => {
  
+        //output the location at console
         console.log(res);
 
     })
@@ -112,6 +134,7 @@ table_arr = mapArrays(date_arr_add, unix_epoch_arr);
     i++;
     }
 
+      //output the result from ISS api
       console.log(json);
 })
  
